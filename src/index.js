9ba7d08a538dd4aed562391d06287cdc91d7a806
sukawatd <sukawatd@thaicom.net>
2020-11-23 17:00:44 +0700

    fix room

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
         socket.join(user.room)
 
         socket.emit('message', generateMessage('Admin', 'Welcome!'))
-        socket.broadcast.to(room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
         io.to(user.room).emit('roomData', {
             room: user.room,
             users: getUsersInRoom(user.room)
